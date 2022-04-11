bd19c08139603eba36e27d1d87a6d5deb4854f0c
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-11 12:33:55 +0200

    Add Exercise 08

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+const product = (... rest) =>{
+    const args = [... rest];
+ 
+    return args.reduce((a,b) => a * b, 1);
+}
+
+console.log(product(1, 2));
+console.log(product(4, 3, 10, 2));
+console.log(product(7));
+console.log(product());
+module.exports = product;
