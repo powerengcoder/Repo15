a0d88b0938af1b583995c8405c761d4d59cfaa4b
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-17 23:27:19 +0200

    Makeup ex 04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -2,12 +2,13 @@ const milili = [10, 25, 4];
 
 function myArr(mili) {
   "use strict";
-  for (let i = 0; i < mili.length; i++) {
-    milili[i] = mili[i];
-  }
-  return milili;
+  mili[0] = 4;
+  mili[1] = 10;
+  mili[2] = 25;
+  
+  return mili;
 }
 
-const mili = [4, 10 , 25];
+const mili = myArr(milili);
 console.log(myArr(mili));
 module.exports = myArr;
